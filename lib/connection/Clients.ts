327fce4595e225bcf5bdd4b1867b2eb98e5ee368
SanzGantengz <81704601+SanzGantengz@users.noreply.github.com>
2022-02-04 21:05:15 +0700

    update Clients.ts

--- lib/connection/Clients.ts
+++ lib/connection/Clients.ts
@@ -1,108 +1,120 @@
-import { WASocket, proto } from '@adiwajshing/baileys'
-import { EventEmitter } from 'node:events'
-import { format } from 'util'
-import { URL } from 'node:url'
-import { IMessage } from '../types/Message'
+import {
+    WASocket,
+    proto
+} from '@adiwajshing/baileys'
+import {
+    EventEmitter
+} from 'node:events'
+import {
+    format
+} from 'util'
+import {
+    URL
+} from 'node:url'
+import {
+    IMessage
+} from '../types/Message'
 import FileType from 'file-type'
 import fetch from 'node-fetch'
 import Fetcher from '../Fetcher'
 import * as fs from 'node:fs'
 
 export type IGetBuff = {
-  result: Buffer | null
-  type: string | null,
-  error: Error | null
+    result: Buffer | null
+    type: string | null,
+    error: Error | null
 }
 
 export class Clients extends EventEmitter {
-  private messages;
-  public data;
-  constructor(public client: WASocket, private message: any){
-    super()
-    this.data = new Array
-    this.messages = message.messages[0]
-    this._loadEvents()
-  }
-  private _loadEvents(){
-    this.client.ws.on('CB:call', call => {
-      this.emit('WS:call', call)
-    })
-    this.client.ev.on('chats.set', chats => {
-      this.emit('CLI:chats', chats)
-    })
-    this.client.ev.on('messages.set', messages => {
-      this.emit('CLI:messages', messages)
-    })
-    this.client.ev.on('groups.update', groups => {
-      this.emit('CLI:groups', groups)
-    })
-    this.client.ev.on('group-participants.update', participants => {
-      let profile = new Array
-      console.log(participants)
-      participants.participants.forEach(async(p) => {
-        let picture = ''
-        try {
-          picture = await this.client.profilePictureUrl(p, 'image')
-        } catch(e) {
-          picture = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6OKCP6J9kSv2vlCoxRWiJWWze3agzcNnxNA&usqp=CAU'
-        }
-        profile.push({
-          participants: p,
-          picture: picture
-        })
-      })
-      this.emit('CLI:group-mem', {
-        id: participants.id,
-        profile,
-        action: participants.action
-      })
-    })
-  }
-  public async reply(text: string | undefined | Buffer, opt?: object | undefined | string, dmic?: boolean): Promise<void>{
-    let buff = Buffer.isBuffer(text) ? await FileType(text) : text;
-    if (buff.mime != undefined){
-      await this.client.sendMessage(this.messages.key.remoteJid, {
-        [buff.mime.split('/')[0]]: buff,
-        disappearingMessagesInChat: dmic
-      }, {
-        quoted: !opt ? this.messages : opt
-      })
-    } else {
-      await this.client.sendMessage(this.messages.key.remoteJid, {
-        text: format(buff)
-      }, {
-        quoted: !opt ? this.messages : opt
-      })
+    private messages;
+    public data;
+    constructor(public client: WASocket, private message: any) {
+        super()
+        this.data = new Array
+        this.messages = message.messages[0]
+        this._loadEvents()
     }
-  }
-  public async fakeReply(text: string, footer: string, jid?: string, dmc?: boolean): Promise<void>{
-      let msg = this.messages;
-      msg.participant = !jid ? '0@s.whatsapp.net' : jid;
-      msg.message.conversation = footer;
-      await this.reply(text, msg, dmc)
-  }
-  public async getBuffer(url: URL): Promise<any>{
-    Fetcher
-      .get(url)
-      .setEncoding('buffer')
-      .end(function(err, buff){
-        if (!err) {
-          return {
-            type: FileType(buff),
-            result: buff,
-            error: null
-          }
+    private _loadEvents() {
+        this.client.ws.on('CB:call', call => {
+            this.emit('WS:call', call)
+        })
+        this.client.ev.on('chats.set', chats => {
+            this.emit('CLI:chats', chats)
+        })
+        this.client.ev.on('messages.set', messages => {
+            this.emit('CLI:messages', messages)
+        })
+        this.client.ev.on('groups.update', groups => {
+            this.emit('CLI:groups', groups)
+        })
+        this.client.ev.on('group-participants.update', participants => {
+            let profile = new Array
+            console.log(participants)
+            participants.participants.forEach(async(p) => {
+                let picture = ''
+                try {
+                    picture = await this.client.profilePictureUrl(p, 'image')
+                } catch(e) {
+                    picture = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6OKCP6J9kSv2vlCoxRWiJWWze3agzcNnxNA&usqp=CAU'
+                }
+                profile.push({
+                    participants: p,
+                    picture: await (picture)
+                })
+            })
+            this.emit('CLI:group-mem', {
+                id: participants.id,
+                profile,
+                action: participants.action
+            })
+        })
+    }
+    public async reply(text: string | undefined | Buffer, opt?: object | undefined | string, dmic?: boolean): Promise < void > {
+        let buff = Buffer.isBuffer(text) ? await FileType(text): text;
+        if (buff.mime != undefined) {
+            await this.client.sendMessage(this.messages.key.remoteJid, {
+                [buff.mime.split('/')[0]]: buff,
+                disappearingMessagesInChat: dmic
+            }, {
+                quoted: !opt ? this.messages: opt
+            })
         } else {
-          return {
-            type: null,
-            result: null,
-            error: err
-          }
+            await this.client.sendMessage(this.messages.key.remoteJid, {
+                text: format(buff)
+            }, {
+                quoted: !opt ? this.messages: opt
+            })
         }
-      })
-  }
-  
-  public loadError(err: Error | string){
-    this.emit('Error', String(err))
-  }
+    }
+    public async fakeReply(text: string, footer: string, jid?: string, dmc?: boolean): Promise < void > {
+        let msg = this.messages;
+        msg.participant = !jid ? '0@s.whatsapp.net': jid;
+        msg.message.conversation = footer;
+        await this.reply(text, msg, dmc)
+    }
+    public async getBuffer(url: URL): Promise < any > {
+        Fetcher
+        .get(url)
+        .setEncoding('buffer')
+        .end(function(err, buff) {
+            if (!err) {
+                return {
+                    type: FileType(buff),
+                    result: buff,
+                    error: null
+                }
+            } else {
+                return {
+                    type: null,
+                    result: null,
+                    error: err
+                }
+            }
+        })
+    }
+
+    public loadError(err: Error | string) {
+        this.emit('Error',
+            String(err))
+    }
 }
