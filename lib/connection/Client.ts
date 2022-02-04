d2c860fcf4f11756facc5a656f84c979d1074140
SanzGantengz <81704601+SanzGantengz@users.noreply.github.com>
2022-02-04 23:04:23 +0700

    update Client.ts

--- lib/connection/Client.ts
+++ lib/connection/Client.ts
@@ -1,43 +1,64 @@
-import makeWASocket, { useSingleFileAuthState, WAMessage, WASocket, DisconnectReason, makeInMemoryStore } from '@adiwajshing/baileys'
-import { EventEmitter } from 'node:events'
-import { Boom } from '@hapi/boom'
+import makeWASocket, {
+    useSingleFileAuthState,
+    WAMessage,
+    WASocket,
+    DisconnectReason,
+    makeInMemoryStore
+} from '@adiwajshing/baileys'
+import {
+    EventEmitter
+} from 'node:events'
+import {
+    Boom
+} from '@hapi/boom'
 import P from 'pino'
 import Handler from '../functions/Handler'
-import { IMessage } from '../types/Message'
+import {
+    IMessage
+} from '../types/Message'
 
-const { state, saveState } = useSingleFileAuthState('../auth.json')
-const store = makeInMemoryStore({ logger: P().child({ level: 'debug', stream: 'store' }) })
+const {
+    state,
+    saveState
+} = useSingleFileAuthState('../auth.json')
+const store = makeInMemoryStore({
+    logger: P().child({
+        level: 'debug', stream: 'store'
+    })
+})
 export class Client extends EventEmitter {
-  public client: WASocket;
-  constructor(){
-    super();
-    (async() => {
-      try {
-        setInterval(() => {
-        store.writeToFile('../auth.json')
-        }, 10_000)
-        this.client = makeWASocket({
-          auth: state,
-          printQRInTerminal: true
-        })
-        store.bind(this.client.ev)
-        this.client.ev.on('messages.upsert', (msg: IMessage) => {
-          try {
-            if (!msg.messages[0]) return;
-            else new Handler(msg, this.client)
-          } catch(e) {
-            return;
-          }
-        })
-        this.client.ev.on('creds.update', saveState)
-        this.client.ev.on('connection.update', update => {
-          if (update.connection == 'close'){
-            this.emit('CL:connection', (update.lastDisconnect.error as Boom)?.output?.statusCode)
-          }
-        })
-      } catch(e) {
-        console.log(e)
-      }
-    })()
-  }
+    public client: WASocket;
+    constructor() {
+        super();
+        (async() => {
+            try {
+                setInterval(() => {
+                    store.writeToFile('../auth.json')
+                }, 10_000)
+                this.client = makeWASocket({
+                    auth: state,
+                    printQRInTerminal: true
+                })
+                store.bind(this.client.ev)
+                this.client.ev.on('messages.upsert', (msg: IMessage) => {
+                    try {
+                        if (!msg.messages[0]) return;
+                        else new Handler(msg, this.client)
+                    } catch(e) {
+                        return;
+                    }
+                })
+                this.client.ev.on('creds.update',
+                    saveState)
+                this.client.ev.on('connection.update',
+                    update => {
+                        if (update.connection == 'close') {
+                            this.emit('CL:connection', (update.lastDisconnect.error as Boom)?.output?.statusCode)
+                        }
+                    })
+            } catch(e) {
+                console.log(e)
+            }
+        })()
+    }
 }
