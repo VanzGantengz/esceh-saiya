f846e9f1c93d49b357cf8768d41f0fd0f85fa353
SanzGantengz <81704601+SanzGantengz@users.noreply.github.com>
2022-02-05 09:33:27 +0700

    update Command.ts

--- lib/functions/Command.ts
+++ lib/functions/Command.ts
@@ -1,25 +1,27 @@
-import { ICommand } from '../types/Command'
+import {
+    ICommand
+} from '../types/Command'
 
 class command {
-  private data = new Map;
-  constructor(){}
-  public addCommands(opt: ICommand){
-    this.data.set('commands', opt)
-  }
-  public addCommand(opt: ICommand){
-    this.data.set('commands', opt)
-  }
-  public addFunctions(opt: any){
-    this.data.set('functions', opt)
-  }
-  public addFunction(opt: any){
-    this.data.set('functions', opt)
-  }
+    private data = new Map;
+    constructor() {}
+    public addCommands(opt: ICommand) {
+        this.data.set('commands', opt)
+    }
+    public addCommand(opt: ICommand) {
+        this.data.set('commands', opt)
+    }
+    public addFunctions(opt: any) {
+        this.data.set('functions', opt)
+    }
+    public addFunction(opt: any) {
+        this.data.set('functions', opt)
+    }
 }
 export const Command: Commands = new command()
 export interface Commands {
-  addCommands(opt: ICommand)
-  addCommand(opt: ICommand)
-  addFunction(opt: any)
-  addFunctions(opt: any)
+    addCommands(opt: ICommand)
+    addCommand(opt: ICommand)
+    addFunction(opt: any)
+    addFunctions(opt: any)
 }
