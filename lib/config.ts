a6af7cae9bd87a7e8f77ae839357c1bfc18e3b7c
SanzGantengz <81704601+SanzGantengz@users.noreply.github.com>
2022-02-06 09:06:25 +0700

    update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,4 +1,4 @@
 export enum Config {
-  MONGGO_URI = 'mongodb+srv://SanZ:asu@cluster0.8o5aa.mongodb.net/data?retryWrites=true&w=majority',
-  prefix = '/'
+    MONGGO_URI = 'mongodb+srv://SanZ:asu@cluster0.8o5aa.mongodb.net/data?retryWrites=true&w=majority',
+    prefix = '/'
 }
